a88da856e7a6bfdd0de55296193767244732888c
Omar Nachar <omarnachar2@gmail.com>
2021-04-16 13:44:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
-function moveDodgerRight() {
-  let RightNumbers = dodger.style.left.replace("px", "");
-  let Right = parseInt(RightNumbers, 10);
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.right = `${left + 1}px`;
+    dodger.style.left = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
